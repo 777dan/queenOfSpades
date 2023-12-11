b4e9918364912289c711873f3fe22ea7040cdb2a
777dan <danya.nikitin7@gmail.com>
2023-12-11 11:03:18 +0200

    added sounds and some dynamic functions

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ let playedCards = [];
 
 let number = 0;
 let isGame = true;
+let turn = 0;
 const cardsField = document.getElementById("cards");
 const realCardsField = document.getElementById("real_cards");
 const playedCardsField = document.getElementById("played_cards");
-const info = document.getElementById("info");
 const rel = document.getElementById("reload");
-
-info.innerHTML = "Take the card!";
+const modal = document.getElementById("myModal");
+const modalContent = document.getElementById("modal-content");
 
 function shuffle(arr) {
     let rand, temp;
@@ -27,27 +27,40 @@ function shuffle(arr) {
 
 shuffle(cards);
 
-cardsField.innerHTML = cards;
+// cardsField.innerHTML = cards;
+
+const playSound = (sound) => {
+    const song = document.getElementById(sound);
+    song.volume = 1;
+    if (song.paused) {
+        song.play();
+    } else {
+        song.pause();
+    }
+}
 
 function allowDrop(event) {
-    if (isGame === true) {
+    if (isGame === true && turn === 0) {
         event.preventDefault();
     }
 }
 
 function drag(event) {
-    if (isGame === true) {
+    if (isGame === true && turn === 0) {
         event.dataTransfer.setData("text", event.target.id);
+        playSound('sound1');
     }
 }
 
 function drop(event) {
-    if (isGame === true) {
+    if (isGame === true && turn === 0) {
         event.preventDefault();
         let data = event.dataTransfer.getData("text");
+        playSound('sound2');
 
         event.target.appendChild(document.getElementById(data));
-        play(cards, data);
+        document.getElementById(data).classList = 'card usedCard';
+        play(data);
     }
 }
 
@@ -60,32 +73,37 @@ function newPlay() {
     return false;
 }
 
-function play(cards, elId) {
+function play(elId) {
     if (!isGame) return;
-    try {
-        if (myMove(elId)) return;
-        setTimeout(computerMove, 300);
-    } catch (ex) {
-        info.innerHTML = ex.message;
-    }
+    if (myMove(elId)) return;
+    setTimeout(computerMove, 300);
 }
 
 function removeCard(number) {
     playedCards.push(cards[number]);
     cards.splice(number, 1);
-    cardsField.innerHTML = cards;
-    generateCards(cards, realCardsField, "", "notUsedCard");
+    // cardsField.innerHTML = cards;
     generateCards(playedCards, playedCardsField, "p", "usedCard");
+    generateCards(cards, realCardsField, "", "notUsedCard");
     addEventCardList();
 }
 
-const checkWin = (player, card) => {
-    console.log(card);
-    if (card === "Q") {
-        info.textContent = `${player} won!`;
+const showModal = (player) => {
+    modalContent.children[0].textContent = `${player} won!`;
+    if (player === "I") {
+        modalContent.style.backgroundColor = 'lightcoral';
+    } else if (player === "You") {
+        modalContent.style.backgroundColor = 'lightgreen';
     }
-    return card === "Q";
+    modal.style.opacity = "1";
+    modal.style.zIndex = "1";
+    setTimeout(() => {
+        modal.style.opacity = "0";
+        modal.style.zIndex = "-1";
+    }, 3000);
 }
+
+const checkWin = (card) => card === "Q";
 
 const animateCards = () => {
     const allCards = document.querySelectorAll('.card');
@@ -97,20 +115,21 @@ const animateCards = () => {
 }
 
 function myMove(elId) {
-    console.log(isGame);
-    if (isGame === true) {
+    if (turn === 0) {
         isGame = false;
         let b = false;
         number = Number(elId.substr(3));
         if (number > cards.length || number < 0) {
             throw new Error("Input error! Try again!");
         }
-        if (checkWin("You", cards[number])) {
+        setTimeout(removeCard.bind(this, number), 200);
+        turn = 1;
+        if (checkWin(cards[number])) {
+            playSound('sound3');
             b = true;
-            setTimeout(removeCard.bind(this, number), 200);
             setTimeout(animateCards, 1000);
+            showModal('You');
         } else {
-            setTimeout(removeCard.bind(null, number), 200);
             isGame = true;
         }
         return b;
@@ -118,23 +137,20 @@ function myMove(elId) {
 }
 
 function computerMove() {
-    if (isGame === true) {
+    if (turn === 1) {
         isGame = false;
         let b = false;
         number = Math.floor(Math.random() * cards.length);
-
-        console.log(number);
-
-        let computerCard = document.getElementById(`rc_${number}`);
-        console.log(computerCard.id);
-
-        if (checkWin("I", cards[number])) {
+        // let computerCard = document.getElementById(`rc_${number}`);
+        setTimeout(removeCard.bind(this, number), 200);
+        turn = 0;
+        if (checkWin(cards[number])) {
+            playSound('sound4');
             b = true;
-            setTimeout(removeCard.bind(this, number), 200);
             setTimeout(animateCards, 1000);
+            showModal('I');
         } else {
             isGame = true;
-            setTimeout(removeCard.bind(this, number), 200);
         }
         return b;
     }
